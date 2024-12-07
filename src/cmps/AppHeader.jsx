7f2453c9f30c76e3d579b2c6f896e325569a9210
Alon Goddard <112505002+ALONGOD@users.noreply.github.com>
2024-12-07 23:12:24 +0200

    Update AppHeader.jsx

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -4,6 +4,7 @@ import { useSelector } from 'react-redux'
 import { showErrorMsg, showSuccessMsg } from '../services/event-bus.service'
 import { logout } from '../store/actions/user.actions'
 
+
 export function AppHeader() {
     const user = useSelector(storeState => storeState.userModule.user)
     const navigate = useNavigate()
