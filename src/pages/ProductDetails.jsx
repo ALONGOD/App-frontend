ca73704b3e066e3cc2e2993749771c81cea0b1cd
Alon Goddarde <alongoddard1@gmail.com>
2024-12-08 11:21:12 +0200

    Add button in product details and change the basic button form styling

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,38 +1,53 @@
-import { useEffect } from 'react'
-import { useParams } from 'react-router-dom'
-import { useSelector } from 'react-redux'
-import { Link } from 'react-router-dom'
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
 
-import { showSuccessMsg, showErrorMsg } from '../services/event-bus.service'
-import { addProductMsg } from '../store/actions/product.actions'
+import { showSuccessMsg, showErrorMsg } from '../services/event-bus.service';
+import { addProductMsg } from '../store/actions/product.actions';
+import { addToCart, removeFromCart } from '../store/actions/cart.actions';
 
 export function ProductDetails() {
-    const { productId } = useParams()
-    const product = useSelector(storeState => storeState.productModule.products.find(product => product._id === +productId))
+    const { productId } = useParams();
+    const product = useSelector(storeState => storeState.productModule.products.find(product => product._id === +productId));
+    const cart = useSelector(storeState => storeState.cartModule.cart);
+    const dispatch = useDispatch();
+    const isInCart = cart.some(item => item._id === product._id);
 
     async function onAddProductMsg(productId) {
         try {
-            await addProductMsg(productId, 'bla bla ' + parseInt(Math.random() * 10))
-            showSuccessMsg(`Product msg added`)
+            await addProductMsg(productId, 'bla bla ' + parseInt(Math.random() * 10));
+            showSuccessMsg(`Product msg added`);
         } catch (err) {
-            showErrorMsg('Cannot add product msg')
+            showErrorMsg('Cannot add product msg');
         }
     }
+
+    const handleAddToCart = () => {
+        dispatch(addToCart(product));
+    };
+
+    const handleRemoveFromCart = () => {
+        dispatch(removeFromCart(product._id));
+    };
 
     return (
         <section className="product-details">
             <Link to="/product">Back to list</Link>
-            <h1>Product Details</h1>
+            <h1>{product.name}</h1>
             {product && (
                 <div>
-                    <h2>{product.name}</h2>
                     <h2>{product.category}</h2>
                     <img src={product.imgUrl} alt={product.name} />
-                    <h4>{product.price} NIS</h4>
+                    <h4>{product.price}₪</h4>
                     <p>{product.description}</p>
+                    {isInCart ? (
+                        <button onClick={handleRemoveFromCart}>Remove from Cart</button>
+                    ) : (
+                        <button onClick={handleAddToCart}>Add to Cart</button>
+                    )}
                 </div>
             )}
-            <button onClick={() => { onAddProductMsg(product._id) }}>Add product msg</button>
         </section>
-    )
+    );
 }
